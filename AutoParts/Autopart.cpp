d83a59fe934e745b8cd3a4da5dc92f1a3a25f0f5
Kevin Wingert <Kevin.Wingert@principia.edu>
2020-05-05 19:00:31 -0500

    Complete4d

--- AutoParts/Autopart.cpp
+++ AutoParts/Autopart.cpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include "Autopart.h"
+#include "BasePart.h"
 
 using namespace std;
 
@@ -11,6 +12,11 @@ AutoPart::AutoPart(int partNo, string partDescription, int partMaker) : BasePart
 		_PartMaker = partMaker;
 		cout << "In AutoPart Destructor: " << _PartDescription << endl;
 	}
+}
+
+AutoPart::~AutoPart()
+{
+	cout << "AutoPart Destructor " << _PartDescription << endl;
 }
 
 /// <summary>
